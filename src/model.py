adb692d9da8782d622c71a5c58098d113f618c1c
gopuvenkat <Gopalakrishnan.V@iiitb.org>
2019-09-16 10:40:41 +0530

    model - BiLSTM + Attention v0

--- src/model.py
+++ src/model.py
@@ -12,6 +12,21 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torchtext
 
+
+class Attention(nn.Module):
+
+    def __init__(self, attention_size):
+        super(Attention, self).__init__()
+        self.attention = nn.Parameter(torch.FloatTensor(attention_size, 1))
+        torch.nn.init.xavier_normal(self.attention)
+
+    def forward(self, x_in):
+        attention_score = torch.matmul(x_in, self.attention).squeeze()
+        attention_score = F.softmax(attention_score, dim=1).view(x_in.shape[0], x_in.shape[1], 1)
+        scored_x = x_in * attention_score
+        condensed_x = torch.sum(scored_x, dim=1)
+        return condensed_x
+
 class MHCAttnNet(nn.Module):
 
     def __init__(self, peptide_embedding, mhc_embedding):
@@ -23,12 +38,25 @@ class MHCAttnNet(nn.Module):
         self.peptide_embedding = peptide_embedding
         self.mhc_embedding = mhc_embedding
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
 
         self.peptide_lstm = nn.LSTM(config.EMBED_DIM, self.hidden_size, num_layers=self.peptide_num_layers, batch_first=True, bidirectional=True)
         self.mhc_lstm = nn.LSTM(config.EMBED_DIM, self.hidden_size, num_layers=self.mhc_num_layers, batch_first=True, bidirectional=True)
-        self.peptide_linear = nn.Linear(2*self.peptide_num_layers*self.hidden_size, config.LINEAR1_OUT)
-        self.mhc_linear = nn.Linear(2*self.mhc_num_layers*self.hidden_size, config.LINEAR1_OUT)
+        # self.pep_attention = Attention(2*self.hidden_size)
+        # self.mhc_attention = Attention(2*self.hidden_size)
+        self.peptide_linear = nn.Linear(2*self.hidden_size, config.LINEAR1_OUT)
+        self.mhc_linear = nn.Linear(2*self.hidden_size, config.LINEAR1_OUT)
         self.out_linear = nn.Linear(config.LINEAR1_OUT*2, config.LINEAR2_OUT)
+
+    def attention(self, rnn_out, state):
+        merged_state = torch.cat([s for s in state], dim=1)
+        merged_state = merged_state.squeeze(0).unsqueeze(2)
+
+        weights = torch.bmm(rnn_out, merged_state)
+        weigths = F.softmax(weights.squeeze(2), dim=1).unsqueeze(2)
+
+        out = torch.bmm(torch.transpose(rnn_out, 1, 2), weigths).squeeze(2)
+        return out
 
     def forward(self, peptide, mhc):
         pep_emb = self.peptide_embedding(peptide)        
@@ -37,14 +65,17 @@ class MHCAttnNet(nn.Module):
 
         pep_lstm_output, (pep_last_hidden_state, pep_last_cell_state) = self.peptide_lstm(pep_emb)
         mhc_lstm_output, (mhc_last_hidden_state, mhc_last_cell_state) = self.mhc_lstm(mhc_emb)
+        # sen_lstm_output = [batch_size, seq_len, 2*hidden_dim]            -> 2 : bidirectional
         # sen_last_hidden_state = [2*num_layers, batch_size, hidden_dim]   -> 2 : bidirectional
-        
-        pep_last_hidden_state = pep_last_hidden_state.transpose(0, 1).contiguous().view(config.batch_size, -1)
-        mhc_last_hidden_state = mhc_last_hidden_state.transpose(0, 1).contiguous().view(config.batch_size, -1)
-        # sen_last_hidden_state = [batch_size, 2*num_layers*hidden_dim]    -> 2 : bidirectional
 
-        pep_linear_out = self.relu(self.peptide_linear(pep_last_hidden_state))
-        mhc_linear_out = self.relu(self.mhc_linear(mhc_last_hidden_state))
+        # pep_attn_linear_inp = self.pep_attention(pep_lstm_output)
+        # mhc_attn_linear_inp = self.mhc_attention(mhc_lstm_output)
+
+        pep_attn_linear_inp = self.attention(pep_lstm_output, pep_last_hidden_state)
+        mhc_attn_linear_inp = self.attention(mhc_lstm_output, mhc_last_hidden_state)
+
+        pep_linear_out = self.relu(self.peptide_linear(pep_attn_linear_inp))
+        mhc_linear_out = self.relu(self.mhc_linear(mhc_attn_linear_inp))
         # sen_linear_out = [batch_size, LINEAR1_OUT]
 
         conc = torch.cat((pep_linear_out, mhc_linear_out), dim=1)
