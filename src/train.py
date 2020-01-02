fix: computation of loss

src/train.py
@@ -24,6 +24,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
     if(val == True):
         for epoch in range(epochs):
             print("Epoch", epoch)
+            total_loss = 0
             y_actual_train = list()
             y_pred_train = list()
             for row in tqdm(train_dl):
@@ -34,9 +35,10 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
                     y_actual_train += list(y_actual.cpu().data.numpy())
                     y_pred_train += list(y_pred_idx.cpu().data.numpy())
                     loss = loss_fn(y_pred, y_actual)
+                    total_loss += loss.item()
                     opt.zero_grad()
                     loss.backward()
-                    optimizer.step()
+                    opt.step()
             accuracy = accuracy_score(y_actual_train, y_pred_train)
             precision = precision_score(y_actual_train, y_pred_train)
             recall = recall_score(y_actual_train, y_pred_train)
@@ -44,7 +46,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             roc_auc = roc_auc_score(y_actual_train, y_pred_train)
             prc_auc = average_precision_score(y_actual_train, y_pred_train)
             # p_train, r_train, _ = precision_recall_curve(y_actual_train, y_pred_train)
-            writer.add_scalar('Loss/train', loss, epoch)
+            writer.add_scalar('Loss/train', total_loss, epoch)
             writer.add_scalar('Accuracy/train', accuracy, epoch)
             writer.add_scalar('Precision/train', precision, epoch)
             writer.add_scalar('Recall/train', recall, epoch)
@@ -54,6 +56,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             writer.add_pr_curve('PR_Curve/train', np.asarray(y_actual_train), np.asarray(y_pred_train))
             print(f"Train - Loss : {loss}, Accuracy : {accuracy}, Precision : {precision}, Recall : {recall}, F1-score : {f1}, ROC_AUC : {roc_auc}, PRC_AUC : {prc_auc}")
 
+            total_loss = 0
             y_actual_val = list()
             y_pred_val = list()
             for row in tqdm(val_dl):
@@ -64,6 +67,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
                     y_actual_val += list(y_actual.cpu().data.numpy())
                     y_pred_val += list(y_pred_idx.cpu().data.numpy())
                     loss = loss_fn(y_pred, y_actual)
+                    total_loss += loss.item()
             accuracy = accuracy_score(y_actual_val, y_pred_val)
             precision = precision_score(y_actual_val, y_pred_val)
             recall = recall_score(y_actual_val, y_pred_val)
@@ -71,7 +75,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             roc_auc = roc_auc_score(y_actual_val, y_pred_val)
             prc_auc = average_precision_score(y_actual_val, y_pred_val)
             # p_val, r_val, _ = precision_recall_curve(y_actual_val, y_pred_val)
-            writer.add_scalar('Loss/val', loss, epoch)
+            writer.add_scalar('Loss/val', total_loss, epoch)
             writer.add_scalar('Accuracy/val', accuracy, epoch)
             writer.add_scalar('Precision/val', precision, epoch)
             writer.add_scalar('Recall/val', recall, epoch)
@@ -83,6 +87,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
     else:
         for epoch in range(epochs):
             print("Epoch", epoch)
+            total_loss = 0
             y_actual_train = list()
             y_pred_train = list()
             for row in tqdm(train_dl):
@@ -93,9 +98,10 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
                     y_actual_train += list(y_actual.cpu().data.numpy())
                     y_pred_train += list(y_pred_idx.cpu().data.numpy())
                     loss = loss_fn(y_pred, y_actual)
+                    total_loss += loss.item()
                     opt.zero_grad()
                     loss.backward()
-                    optimizer.step()
+                    opt.step()
             for row in tqdm(val_dl):
                 if row.batch_size == config.batch_size:
                     y_pred = model(row.peptide, row.mhc_amino_acid)
@@ -104,9 +110,10 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
                     y_actual_train += list(y_actual.cpu().data.numpy())
                     y_pred_train += list(y_pred_idx.cpu().data.numpy())
                     loss = loss_fn(y_pred, y_actual)
+                    total_loss += loss.item()
                     opt.zero_grad()
                     loss.backward()
-                    optimizer.step()
+                    opt.step()
             accuracy = accuracy_score(y_actual_train, y_pred_train)
             precision = precision_score(y_actual_train, y_pred_train)
             recall = recall_score(y_actual_train, y_pred_train)
@@ -114,7 +121,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             roc_auc = roc_auc_score(y_actual_train, y_pred_train)
             prc_auc = average_precision_score(y_actual_train, y_pred_train)
             # p_val, r_val, _ = precision_recall_curve(y_actual_train, y_pred_train)
-            writer.add_scalar('Loss/train', loss, epoch)
+            writer.add_scalar('Loss/train', total_loss, epoch)
             writer.add_scalar('Accuracy/train', accuracy, epoch)
             writer.add_scalar('Precision/train', precision, epoch)
             writer.add_scalar('Recall/train', recall, epoch)
@@ -124,6 +131,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             writer.add_pr_curve('PR_Curve/train', np.asarray(y_actual_train), np.asarray(y_pred_train))
             print(f"Train - Loss : {loss}, Accuracy : {accuracy}, Precision : {precision}, Recall : {recall}, F1-score : {f1}, ROC_AUC : {roc_auc}, PRC_AUC : {prc_auc}")
 
+            total_loss = 0
             y_actual_test = list()
             y_pred_test = list()
             for row in tqdm(test_dl):
@@ -134,6 +142,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
                     y_actual_test += list(y_actual.cpu().data.numpy())
                     y_pred_test += list(y_pred_idx.cpu().data.numpy())
                     loss = loss_fn(y_pred, y_actual)
+                    total_loss += loss.item()
             accuracy = accuracy_score(y_actual_test, y_pred_test)
             precision = precision_score(y_actual_test, y_pred_test)
             recall = recall_score(y_actual_test, y_pred_test)
@@ -141,7 +150,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             roc_auc = roc_auc_score(y_actual_test, y_pred_test)
             prc_auc = average_precision_score(y_actual_test, y_pred_test)
             # p_val, r_val, _ = precision_recall_curve(y_actual_test, y_pred_test)
-            writer.add_scalar('Loss/test', loss, epoch)
+            writer.add_scalar('Loss/test', total_loss, epoch)
             writer.add_scalar('Accuracy/test', accuracy, epoch)
             writer.add_scalar('Precision/test', precision, epoch)
             writer.add_scalar('Recall/test', recall, epoch)
@@ -151,7 +160,7 @@ def fit(model, train_dl, val_dl, test_dl, loss_fn, opt, epochs, device, val=Fals
             writer.add_pr_curve('PR_Curve/test', np.asarray(y_actual_test), np.asarray(y_pred_test))
             print(f"Test - Loss : {loss}, Accuracy : {accuracy}, Precision : {precision}, Recall : {recall}, F1-score : {f1}, ROC_AUC : {roc_auc}, PRC_AUC : {prc_auc}")
 
-            if epoch % 1 == 0:
+            if epoch % config.ckpt_num == 0:
                 torch.save(model.state_dict(), config.model_name)
 
 