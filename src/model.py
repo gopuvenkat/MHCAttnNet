865619c1af8d1186fb910ddcd7d5036aeada493c
gopuvenkat <Gopalakrishnan.V@iiitb.org>
2020-01-04 10:35:55 +0530

    non attention model

--- src/model.py
+++ src/model.py
@@ -64,8 +64,8 @@ class MHCAttnNet(nn.Module):
         self.peptide_attn = Attention(2*self.hidden_size, config.PEPTIDE_LENGTH, config.CONTEXT_DIM)
         self.mhc_attn = Attention(2*self.hidden_size, config.MHC_AMINO_ACID_LENGTH, config.CONTEXT_DIM)
 
-        self.peptide_linear = nn.Linear(2*self.hidden_size, config.LINEAR1_OUT)
-        self.mhc_linear = nn.Linear(2*self.hidden_size, config.LINEAR1_OUT)
+        self.peptide_linear = nn.Linear(2*self.hidden_size*self.peptide_num_layers, config.LINEAR1_OUT)
+        self.mhc_linear = nn.Linear(2*self.hidden_size*self.mhc_num_layers, config.LINEAR1_OUT)
         self.hidden_linear = nn.Linear(2*config.LINEAR1_OUT, config.LINEAR1_OUT)
         self.out_linear = nn.Linear(config.LINEAR1_OUT, config.LINEAR2_OUT)
 
@@ -79,12 +79,16 @@ class MHCAttnNet(nn.Module):
         # sen_lstm_output = [batch_size, seq_len, 2*hidden_dim]            -> 2 : bidirectional
         # sen_last_hidden_state = [2*num_layers, batch_size, hidden_dim]   -> 2 : bidirectional
 
-        pep_attn_linear_inp = self.peptide_attn(pep_lstm_output)
-        mhc_attn_linear_inp = self.mhc_attn(mhc_lstm_output)
+        # pep_attn_linear_inp = self.peptide_attn(pep_lstm_output)
+        # mhc_attn_linear_inp = self.mhc_attn(mhc_lstm_output)
         # sen_attn_linear_inp = [batch_size, 2*hidden_dim]                 -> 2 : bidirectional
-        
-        pep_linear_out = self.relu(self.peptide_linear(pep_attn_linear_inp))
-        mhc_linear_out = self.relu(self.mhc_linear(mhc_attn_linear_inp))
+
+        pep_last_hidden_state = pep_last_hidden_state.transpose(0, 1).contiguous().view(config.batch_size, -1)
+        mhc_last_hidden_state = mhc_last_hidden_state.transpose(0, 1).contiguous().view(config.batch_size, -1)
+        # sen_last_hidden_state = [batch_size, 2*num_layers*hidden_dim]    -> 2 : bidirectional
+
+        pep_linear_out = self.relu(self.peptide_linear(pep_last_hidden_state))
+        mhc_linear_out = self.relu(self.mhc_linear(mhc_last_hidden_state))
         # sen_linear_out = [batch_size, LINEAR1_OUT]
 
         conc = torch.cat((pep_linear_out, mhc_linear_out), dim=1)
